977721941b388c40f3997031bf1c35a5b784f3d9
Dominik Broj <dominik.broj@dataart.com>
2017-01-31 17:03:50 +0100

    put feed in separate component

--- client/app/feature_modules/ch-chat/index.js
+++ client/app/feature_modules/ch-chat/index.js
@@ -7,12 +7,13 @@ import { ChChatComponent } from './components/ch-chat/ch-chat.component';
 import { ChLobbyComponent } from './components/ch-lobby/ch-lobby.component';
 import { ChRoomComponent } from './components/ch-room/ch-room.component';
 import { ChMessagesWindowComponent } from './components/ch-messages-window/ch-messages-window.component';
+import { ChFeedComponent } from './components/ch-feed/ch-feed.component';
 import { ChRoomService } from './services/ch-room.service';
 import { ChChatService } from './services/ch-chat.service';
 
 @NgModule({
   imports: [ CommonModule, RouterModule, FormsModule, ChCommonModule ],
-  declarations: [ ChChatComponent, ChLobbyComponent, ChRoomComponent, ChMessagesWindowComponent ],
+  declarations: [ ChChatComponent, ChLobbyComponent, ChRoomComponent, ChMessagesWindowComponent, ChFeedComponent ],
   exports:      [ ChChatComponent, ChLobbyComponent, ChRoomComponent ],
   providers: [ ChRoomService, ChChatService ]
 })
