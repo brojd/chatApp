70b93c2ce1814d59ca992b3511ae7edd269f88f9
Dominik Broj <dominik.broj@dataart.com>
2017-02-06 13:09:56 +0100

    create store for users that joined room

---
+++ server/usersInRooms.js
@@ -0,0 +1,34 @@
+let usersInRooms = [];
+
+const generateRooms = (dbRooms) => {
+  usersInRooms = dbRooms.map((room) => {
+    return { id: room._id, users: [] }
+  });
+};
+
+const getAllRooms = () => usersInRooms;
+
+const getRoom = (roomId) => usersInRooms.find((room) => room.id == roomId);
+
+const addUserToRoom = (roomId, nickname) => {
+  usersInRooms.map((room) => {
+    if (room.id == roomId) {
+      room.users = [ ...room.users, nickname ];
+      return room;
+    }
+    return room;
+  });
+};
+
+const deleteUserFromRoom = (roomId, nickname) => {
+  let usersInRoomsCopy = usersInRooms.slice();
+  usersInRooms.map((room) => {
+    if (room.id == roomId) {
+      room.users = room.users.filter((user) => user !== nickname);
+      return room;
+    }
+    return room;
+  });
+};
+
+module.exports = { generateRooms, getAllRooms, getRoom, addUserToRoom, deleteUserFromRoom };
